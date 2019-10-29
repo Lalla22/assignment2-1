a00a7424f9ca6bdc6a96104f3648bc543ed1f79b
Lalla Sankara <47159210+Lalla22@users.noreply.github.com>
2019-10-29 00:39:04 -0400

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,42 +1,52 @@
-/******************************************************************************
- *  _Example commands for running this file_
- *  Compilation:  javac -Xlint Palindrome.java
- *  Execution:    java Palindrome
- *
- *  Example code reverses the input array. Edit this file
- *  to check whether the input is a palindrome.
- *
- ******************************************************************************/
 
-
-import java.util.Arrays;
+/**
+ * Implement a function to check if an input is a palindrome. 
+ *
+ * @author Lalla Sankara
+ * @version 10/29/2019
+ */
 import java.util.Stack;
-
-public class Palindrome {
-
-  // Sample input
-  public static int[] input = {0,1,2,1,0};
-
-  public static void main(String[] args){
-    // Display Input
-    System.out.println("Input:\t" + Arrays.toString(input));
-
-    // Create an empty stack
-    Stack<Integer> stack = new Stack<Integer>();
-    for(int i=0; i<input.length; i++){
-      // One by one push all characters of string to stack.
-      stack.push(input[i]);
+import java.util.Scanner;
+public class Palindrome.java;
+{
+    public static void main (String [] args)
+    {
+     //Write what you want user to type into scanner   
+     System.out.print("Enter String");
+     
+     //Implepment scanner
+     Scanner in = new Scanner(System.in);
+     String inputString = in.nextLine();
+     
+     //create new stack 
+     Stack stack = new Stack(); 
+    
+     //checks the string and push all characters of string to stack
+     for (int i=0;i<inputString.length();  i++) {
+         stack.push(inputString.charAt(i));
+        }
+        
+     //declaring the reverse input string
+     String reverseString = "";
+     
+     //the resverse input string the checks palindrome backwards 
+      while (!stack.isEmpty()) {
+            reverseString = reverseString+stack.pop();
+        }
     }
-
-    // Save the reversed input string
-    String reversed = "";
-    while(!stack.empty()){
-      //One by one pop all characters from stack and put them back to string.
-      reversed += Integer.toString(stack.pop());
-    }
-
-    // Display reversed as string array
-    System.out.println("Output:\t" + Arrays.toString(reversed.split("")));
-  } // end main
-
-}// end class Palindrome
+    public static boolean palindrome(String inputString, String reverseString)
+     {
+         //boolean method that sees if inputString is equal to reverseString
+         // and is a paldindrome
+         //returns back true or false if it is or it is not a palindrome. 
+         
+         if (inputString.equals(reverseString))
+         {
+             return true;
+            }
+            else
+            return false;
+         
+     }
+     
+}
